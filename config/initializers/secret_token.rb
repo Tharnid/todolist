5977621c16aebef63807ee9259d1b646c6b890b6
Tharnid <jeffmsmithdev@gmail.com>
2011-07-23 16:42:39 -0400

    Updated readme file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = 'ee9b1d11220aac47689e2d861f2030f0aa362fe068d4c66ca1b66965f8e6a61b97f783c192c81fc417e6f7aaa351a6bfaf39034f043bc2bc2ed90560cb6a5d78'
